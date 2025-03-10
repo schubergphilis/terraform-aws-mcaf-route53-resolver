3d6c0c08d69f781319d3d9dcca6d6210f921c3d5
Anurag Gautam <anurag.gtm@gmail.com>
2025-03-10 15:10:33 +0100

    update examples

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -2,6 +2,20 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+# vpc
+module "vpc" {
+  source              = "github.com/schubergphilis/terraform-aws-mcaf-vpc"
+  name                = "test"
+  cidr_block          = "192.168.0.0/24"
+  availability_zones  = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  public_subnet_bits  = 28
+  private_subnet_bits = 28
+
+  tags = {
+    environment = "test"
+  }
+}
+
 # Example to create the Route53 Inbound Resolver.
 module "inbound_resolver_endpoints" {
   source = "../.."
